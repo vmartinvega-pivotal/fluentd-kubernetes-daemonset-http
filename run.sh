d65cb7d346d8aa3c8dba93dffa9c67ec6dac8f43
vicente <vicente.martin.vega@gmail.com>
2020-08-10 15:07:55 +0200

    Modified fluend flow

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker run -p 9880:9880 \
--v /home/vicente/Projects/fluentd/fluentd-kubernetes-daemonset/docker-image/v1.11/debian-http/conf:/fluentd/etc \
+-v /home/vicente/Projects/fluentd-kubernetes-daemonset-http/conf:/fluentd/etc \
 -v /tmp:/tmp \
 -e JENKINS_PRODUCDER_URL=http://192.168.99.1:8083 \
 -e FLUENTD_CONF=fluent.conf \
