5e1917f55200a8078850c32264b64d78ff9013bd
vmartinvega-pivotal <vmartinvega-pivotal@users.noreply.github.com>
2020-08-16 18:58:30 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 docker run -p 9880:9880 \
--v /home/vicente/Projects/fluentd-kubernetes-daemonset-http/conf:/fluentd/etc \
+-v /home/vicente/Projects/fluentd-kubernetes-http/conf:/fluentd/etc \
 -v /tmp:/tmp \
 -e JENKINS_PRODUCDER_URL=http://192.168.99.1:8083 \
 -e FLUENTD_CONF=fluent.conf \
-vmartinvega/fluentd-kubernetes-daemonset:v1-debian-http
+vmartinvega/fluentd-kubernetes-http:v1-debian
